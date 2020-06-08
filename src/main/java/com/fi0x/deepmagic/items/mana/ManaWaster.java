ee527860edd8f95ecc99da1d270146f98f2eb608
Fi0x <felpech@aol.com>
2020-06-08 16:29:35 +0200

    Changed Mana Waster to not be consumed when used
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/items/mana/ManaWaster.java
+++ src/main/java/com/fi0x/deepmagic/items/mana/ManaWaster.java
@@ -7,7 +7,6 @@ import com.fi0x.deepmagic.mana.player.PlayerMana;
 import com.fi0x.deepmagic.mana.player.PlayerProperties;
 import com.fi0x.deepmagic.util.IHasModel;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
