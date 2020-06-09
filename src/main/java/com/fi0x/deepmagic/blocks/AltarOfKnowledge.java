3d82f3e046754b306dd2c146e135ebb7f56bc328
Fi0x <felpech@aol.com>
2020-06-09 10:32:40 +0200

    Added Altar of knowledge
    Signed-off-by: Fi0x <felpech@aol.com>

---
+++ src/main/java/com/fi0x/deepmagic/blocks/AltarOfKnowledge.java
@@ -0,0 +1,34 @@
+package com.fi0x.deepmagic.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+
+public class AltarOfKnowledge extends BlockBase
+{
+    public AltarOfKnowledge(String name, Material material)
+    {
+        super(name, material);
+        setSoundType(SoundType.STONE);
+        setHardness(3.0F);
+        setResistance(20.0F);
+        setHarvestLevel("pickaxe", 3);
+        setLightLevel(1.0F);
+        setDefaultSlipperiness(0.7F);
+    }
+
+    @Override
+    public boolean onBlockActivated(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state, @Nonnull EntityPlayer playerIn, @Nonnull EnumHand hand, @Nonnull EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        //TODO: Open Skill-tree of player
+        System.out.println("THE ALTAR WAS ACTIVATED");
+        return true;
+    }
+}
