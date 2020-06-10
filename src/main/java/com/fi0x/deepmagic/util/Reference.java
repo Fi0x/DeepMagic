8d7f437210de13b0dfa71d48ce9f4b82c64153e2
Fi0x <felpech@aol.com>
2020-06-10 17:45:08 +0200

    New Branch
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/util/Reference.java
+++ src/main/java/com/fi0x/deepmagic/util/Reference.java
@@ -8,9 +8,15 @@ public class Reference
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.fi0x.deepmagic.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.fi0x.deepmagic.proxy.CommonProxy";
-	
+
+	//Dimension IDs
 	public static final int DIMENSION_ID = 42;
+
+	//Entity IDs
 	public static final int ENTITY_INSANITY_COW = 1764;
 	public static final int ENTITY_SPIKY_SLIME = 1765;
 	public static final int ENTITY_HOVERING_ORB = 1766;
+
+	//Gui IDs
+	public static final int GUI_SKILLTREE = 74088;
 }
