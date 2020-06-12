29e8a382923744b366e7856bce1af212e0ef1d36
Fi0x <felpech@aol.com>
2020-06-12 18:51:17 +0200

    Added Giant-class
    Signed-off-by: Fi0x <felpech@aol.com>

---
+++ src/main/java/com/fi0x/deepmagic/entities/EntityGiant.java
@@ -0,0 +1,12 @@
+package com.fi0x.deepmagic.entities;
+
+import net.minecraft.entity.monster.EntityGiantZombie;
+import net.minecraft.world.World;
+
+public class EntityGiant extends EntityGiantZombie
+{
+    public EntityGiant(World worldIn)
+    {
+        super(worldIn);
+    }
+}
