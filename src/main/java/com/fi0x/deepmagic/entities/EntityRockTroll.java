abd9d73a1d6483486d223b91a90780edf7f8043d
Fi0x <felpech@aol.com>
2020-06-14 14:03:50 +0200

    Updated Rock Troll so it won't despawn in peaceful Added Rock Troll Cave to insanity dimension Added variable to check rock trolls defence state
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/entities/EntityRockTroll.java
+++ src/main/java/com/fi0x/deepmagic/entities/EntityRockTroll.java
@@ -3,26 +3,35 @@ package com.fi0x.deepmagic.entities;
 import com.fi0x.deepmagic.util.handlers.LootTableHandler;
 import com.fi0x.deepmagic.util.handlers.SoundsHandler;
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
-import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class EntityRockTroll extends EntityMob
+public class EntityRockTroll extends EntityCreature
 {
+    private boolean defenceState;
+
     public EntityRockTroll(World worldIn)
     {
         super(worldIn);
-        setSize(2F, 3F);
+        setSize(2F, 2.5F);
         this.isImmuneToFire = true;
+        defenceState = false;
+        enablePersistence();
     }
 
     @Override
@@ -39,10 +48,12 @@ public class EntityRockTroll extends EntityMob
     protected void applyEntityAttributes()
     {
         super.applyEntityAttributes();
+        getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
+
+        getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(32);
         getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(100);
         getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.27);
         getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(8);
-        getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(32);
     }
     @Override
     public void onLivingUpdate()
@@ -85,5 +96,65 @@ public class EntityRockTroll extends EntityMob
     protected ResourceLocation getLootTable()
     {
         return LootTableHandler.ROCK_TROLL;
+    }
+
+    @Override
+    public boolean getIsInvulnerable()
+    {
+        return defenceState;
+    }
+
+    @Override
+    public boolean attackEntityAsMob(@Nonnull Entity entityIn)
+    {
+        float f = (float)this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue();
+        int i = 0;
+
+        if (entityIn instanceof EntityLivingBase)
+        {
+            f += EnchantmentHelper.getModifierForCreature(this.getHeldItemMainhand(), ((EntityLivingBase)entityIn).getCreatureAttribute());
+            i += EnchantmentHelper.getKnockbackModifier(this);
+        }
+
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), f);
+
+        if (flag)
+        {
+            if (i > 0 && entityIn instanceof EntityLivingBase)
+            {
+                ((EntityLivingBase)entityIn).knockBack(this, (float)i * 0.5F, MathHelper.sin(this.rotationYaw * 0.017453292F), -MathHelper.cos(this.rotationYaw * 0.017453292F));
+                this.motionX *= 0.6D;
+                this.motionZ *= 0.6D;
+            }
+
+            int j = EnchantmentHelper.getFireAspectModifier(this);
+
+            if (j > 0)
+            {
+                entityIn.setFire(j * 4);
+            }
+
+            if (entityIn instanceof EntityPlayer)
+            {
+                EntityPlayer entityplayer = (EntityPlayer)entityIn;
+                ItemStack itemstack = this.getHeldItemMainhand();
+                ItemStack itemstack1 = entityplayer.isHandActive() ? entityplayer.getActiveItemStack() : ItemStack.EMPTY;
+
+                if (!itemstack.isEmpty() && !itemstack1.isEmpty() && itemstack.getItem().canDisableShield(itemstack, itemstack1, entityplayer, this) && itemstack1.getItem().isShield(itemstack1, entityplayer))
+                {
+                    float f1 = 0.25F + (float)EnchantmentHelper.getEfficiencyModifier(this) * 0.05F;
+
+                    if (this.rand.nextFloat() < f1)
+                    {
+                        entityplayer.getCooldownTracker().setCooldown(itemstack1.getItem(), 100);
+                        this.world.setEntityState(entityplayer, (byte)30);
+                    }
+                }
+            }
+
+            this.applyEnchantments(this, entityIn);
+        }
+
+        return flag;
     }
 }
--- src/main/java/com/fi0x/deepmagic/world/generators/WorldGenCustomStructures.java
+++ src/main/java/com/fi0x/deepmagic/world/generators/WorldGenCustomStructures.java
@@ -19,6 +19,7 @@ public class WorldGenCustomStructures implements IWorldGenerator
 {
 	public static final ModWorldGenStructure MAGE_HOUSE = new ModWorldGenStructure("mage_house");
 	public static final ModWorldGenStructure MAGE_HOUSE_SMALL = new ModWorldGenStructure("mage_house_small");
+	public static final ModWorldGenStructure INSANITY_ROCK_TROLL_CAVE = new ModWorldGenStructure("insanity_rock_troll_cave");
 	
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
@@ -27,6 +28,7 @@ public class WorldGenCustomStructures implements IWorldGenerator
 		{
 			generateStructure(MAGE_HOUSE_SMALL, world, random, chunkX, chunkZ, -4, 500, BiomeInsanity.class);//increase the chance number to decrease spawn rate
 			generateStructure(MAGE_HOUSE, world, random, chunkX, chunkZ, -2, 1000, BiomeInsanity.class);//increase the chance number to decrease spawn rate
+			generateStructure(INSANITY_ROCK_TROLL_CAVE, world, random, chunkX, chunkZ, -1, 500, BiomeInsanity.class);
 		}
 	}
 	
