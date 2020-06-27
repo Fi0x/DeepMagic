ad23a740a61d420d44ec8b73bf9d95814e03766b
Fi0x <felpech@aol.com>
2020-06-27 22:29:57 +0200

    Added function to Ranged mog annihilation spell
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/items/spells/SpellRangedMobAnnihilation.java
+++ src/main/java/com/fi0x/deepmagic/items/spells/SpellRangedMobAnnihilation.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
@@ -15,10 +16,12 @@ import javax.annotation.Nonnull;
 
 public class SpellRangedMobAnnihilation extends SpellBase
 {
+    int radius;
     public SpellRangedMobAnnihilation(String name, int tier)
     {
         super(name, tier);
         this.manaCost = 100;
+        this.radius = 10;
     }
 
     @Nonnull
@@ -31,15 +34,29 @@ public class SpellRangedMobAnnihilation extends SpellBase
         assert playerMana != null;
         if(playerMana.getSpellTier() >= tier)
         {
-            if(playerMana.removeMana(manaCost * Math.pow(2, tier - 1), playerIn))
+            if(playerMana.removeMana(manaCost * Math.pow(2, tier - 4), playerIn))
             {
                 if((Math.random() * playerMana.spellCastSkill) > tier)
                 {
-                    //TODO: raycast
-                    //TODO: kill all mobs in area
+                    //TODO: Change BlockPos to raycast
+                    BlockPos pos = new BlockPos(playerIn.getPosition());
+                    createExplosions(worldIn, playerIn, pos, this.radius);
                 } else playerIn.sendMessage(new TextComponentString(TextFormatting.RED + "The spell didn't work"));
             } else playerIn.sendMessage(new TextComponentString(TextFormatting.RED + "You don't have enough mana"));
         } else playerIn.sendMessage(new TextComponentString(TextFormatting.RED + "Your spell tier is not high enough"));
         return new ActionResult<>(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+    }
+
+    private void createExplosions(World world, EntityPlayer player, BlockPos pos, int radius)
+    {
+        for(int x = -radius; x <= radius; x++)
+        {
+            for(int z = -radius; z <= radius; z++)
+            {
+                world.createExplosion(player, pos.getX() + x, pos.getY() + 1, pos.getZ() + z, 1, false);
+                world.createExplosion(player, pos.getX() + x, pos.getY(), pos.getZ() + z, 1, false);
+                world.createExplosion(player, pos.getX() + x, pos.getY() -1, pos.getZ() + z, 1, false);
+            }
+        }
     }
 }
