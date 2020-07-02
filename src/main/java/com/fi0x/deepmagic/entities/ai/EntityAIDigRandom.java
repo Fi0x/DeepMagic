a2546c25e6c582b4cee6bf5a155ade62dc86d80b
Fi0x <felpech@aol.com>
2020-07-02 23:32:17 +0200

    Added basic function to digRandom ai
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/entities/ai/EntityAIDigRandom.java
+++ src/main/java/com/fi0x/deepmagic/entities/ai/EntityAIDigRandom.java
@@ -2,6 +2,7 @@ package com.fi0x.deepmagic.entities.ai;
 
 import com.fi0x.deepmagic.entities.EntityDwarf;
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class EntityAIDigRandom extends EntityAIBase
@@ -25,11 +26,19 @@ public class EntityAIDigRandom extends EntityAIBase
     @Override
     public void startExecuting()
     {
+        creature.isMining = true;
+        BlockPos pos = getRandomBlock();
+        world.getBlockState(pos).getBlock().dropBlockAsItem(world, pos, world.getBlockState(pos).getBlock().getDefaultState(), 1);
     }
 
     @Override
     public boolean shouldContinueExecuting()
     {
         return false;
+    }
+
+    private BlockPos getRandomBlock()
+    {
+        return creature.getPosition().add(Math.random() * 3 - 1, Math.random() * 2, Math.random() * 3 - 1);
     }
 }
