c765acb42fbf9aec7858f5ee444bd68edb61c5f6
Fi0x <felpech@aol.com>
2020-07-02 10:19:08 +0200

    Added AI and stats to worm
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/entities/EntityWorm.java
+++ src/main/java/com/fi0x/deepmagic/entities/EntityWorm.java
@@ -4,6 +4,10 @@ import com.fi0x.deepmagic.util.handlers.LootTableHandler;
 import com.fi0x.deepmagic.util.handlers.SoundsHandler;
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
@@ -18,17 +22,23 @@ public class EntityWorm extends EntityCreature
     public EntityWorm(World worldIn)
     {
         super(worldIn);
-        setSize(0.7F, 0.125F);
+        setSize(0.65F, 0.125F);
     }
 
     @Override
     protected void initEntityAI()
     {
+        this.tasks.addTask(1, new EntityAIPanic(this, 2));
+        this.tasks.addTask(2, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(3, new EntityAILookIdle(this));
     }
     @Override
     protected void applyEntityAttributes()
     {
         super.applyEntityAttributes();
+        getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(32);
+        getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(5);
+        getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.2);
     }
 
     @Override
