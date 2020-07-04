32ace5439252e4cf2eeed1cc646d303f66ff70b6
Fi0x <felpech@aol.com>
2020-07-04 23:53:27 +0200

    Started implementation of storing item from mining ai in chest
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/entities/ai/EntityAIMining.java
+++ src/main/java/com/fi0x/deepmagic/entities/ai/EntityAIMining.java
@@ -5,6 +5,9 @@ import net.minecraft.block.BlockChest;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.RandomPositionGenerator;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -12,15 +15,17 @@ import net.minecraft.world.World;
 public class EntityAIMining extends EntityAIBase
 {
     protected final int executionChance;
-    protected final EntityCreature entity;
     protected final World world;
+    protected final EntityCreature entity;
+    protected final double speed;
+    protected final int searchRange = 30;
+    protected final float probability;
+    protected final int maxExecutionHeight = 50;
     protected double x;
     protected double y;
     protected double z;
-    protected final double speed;
-    protected final float probability;
     protected BlockPos chestPos;
-    private final int searchRange = 30;
+    protected TileEntityChest chestEntity;
 
     public EntityAIMining(EntityDwarf entity)
     {
@@ -28,7 +33,7 @@ public class EntityAIMining extends EntityAIBase
     }
     public EntityAIMining(EntityDwarf entity, int speed)
     {
-        this(entity, speed, 120);
+        this(entity, speed, 50);
     }
     public EntityAIMining(EntityDwarf entity, int speed, int executionChance)
     {
@@ -43,8 +48,8 @@ public class EntityAIMining extends EntityAIBase
     @Override
     public boolean shouldExecute()
     {
-        if (this.entity.getIdleTime() >= 100) return false;
-        if (this.entity.getRNG().nextInt(this.executionChance) != 0) return false;
+        if (this.entity.getIdleTime() >= 100 || this.entity.getRNG().nextInt(this.executionChance) != 0) return false;
+        if (entity.posY > maxExecutionHeight) return false;
 
         Vec3d vec3d = this.getPosition();
 
@@ -62,26 +67,42 @@ public class EntityAIMining extends EntityAIBase
     public void startExecuting()
     {
         chestPos = findChest(entity.getPosition());
+        TileEntity te = world.getTileEntity(chestPos);
+        if(te instanceof TileEntityChest) chestEntity = (TileEntityChest) te;
+
         this.entity.getNavigator().tryMoveToXYZ(this.x, this.y, this.z, this.speed);
+        //TODO: get a random position that might be a wall
     }
 
     @Override
     public boolean shouldContinueExecuting()
     {
+        //TODO: start digging a tunnel to the new destination
         if(entity.getNavigator().noPath())
         {
-            BlockPos pos = getRandomBlock();
-            world.getBlockState(pos).getBlock().dropBlockAsItem(world, pos, world.getBlockState(pos).getBlock().getDefaultState(), 1);
-            world.setBlockToAir(pos);
-            pos = pos.add(0, (Math.random() * 3) - 1, 0);
-            world.getBlockState(pos).getBlock().dropBlockAsItem(world, pos, world.getBlockState(pos).getBlock().getDefaultState(), 1);
-            world.setBlockToAir(pos);
+            BlockPos pos = getRandomSurroundingBlock();
+            digAtBlockPos(pos);
+            digAtBlockPos(pos.add(0, (Math.random() * 3) - 1, 0));
             return false;
         }
         return true;
     }
 
-    protected BlockPos getRandomBlock()
+    protected void digAtBlockPos(BlockPos pos)
+    {
+        //TODO: find out which item will be dropped by the block at specified position
+
+        int currentSlot = 0;
+        ItemStack currentSlotContent = chestEntity.getStackInSlot(currentSlot);
+        while(currentSlotContent != ItemStack.EMPTY || currentSlot < chestEntity.getSizeInventory())
+        {
+            //TODO: check if dropped item can be placed in current inventory-slot
+        }
+        //TODO: store Item in inventory slot if slot is not null
+        world.getBlockState(pos).getBlock().dropBlockAsItem(world, pos, world.getBlockState(pos).getBlock().getDefaultState(), 1);
+        world.setBlockToAir(pos);
+    }
+    protected BlockPos getRandomSurroundingBlock()
     {
         int x = 0;
         int z = 0;
@@ -96,14 +117,16 @@ public class EntityAIMining extends EntityAIBase
     {
         if (entity.isInWater())
         {
-            Vec3d vec3d = RandomPositionGenerator.getLandPos(this.entity, 15, 7);
-            return vec3d == null ? RandomPositionGenerator.findRandomTarget(this.entity, 10, 7) : vec3d;
+            Vec3d vec3d = RandomPositionGenerator.getLandPos(this.entity, searchRange, searchRange / 3);
+            if(vec3d == null) return RandomPositionGenerator.findRandomTarget(this.entity, searchRange, searchRange / 3);
+            else return vec3d;
         }
-        return this.entity.getRNG().nextFloat() >= this.probability ? RandomPositionGenerator.getLandPos(this.entity, 10, 7) : RandomPositionGenerator.findRandomTarget(this.entity, 10, 7);
+        if(entity.getRNG().nextFloat() >= this.probability) return RandomPositionGenerator.getLandPos(this.entity, searchRange, searchRange / 3);
+        else return RandomPositionGenerator.findRandomTarget(this.entity, searchRange, searchRange / 3);
     }
     protected BlockPos findChest(BlockPos pos)
     {
-        int height = 5;
+        int height = searchRange / 4;
 
         for(int range = 0; range <= searchRange; range++)
         {
@@ -129,7 +152,6 @@ public class EntityAIMining extends EntityAIBase
                 }
             }
         }
-        System.out.println("No chest found");
         return null;
     }
 }
