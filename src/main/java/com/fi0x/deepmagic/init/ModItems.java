c5fcf16cf938d411f2248d2d4750730afb6ade21
Fi0x <58072122+Fi0x@users.noreply.github.com>
2020-10-29 18:31:46 +0100

    Bug fix (#156)
    * Added Skillpoint Item
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Fixed MaxMana Hud bug
    Fixed Skillpoint item removal
    
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/init/ModItems.java
+++ src/main/java/com/fi0x/deepmagic/init/ModItems.java
@@ -35,6 +35,7 @@ public class ModItems
 	public static final Item TELEPORTATION_CRYSTAL = new TeleportationCrystal("teleportation_crystal");
 	public static final Item MANA_BOOSTER = new ManaBooster("mana_booster");
 	public static final Item DEMON_CRYSTAL = new DemonCrystal("demon_crystal");
+	public static final Item SKILLPOINT = new Skillpoint("skillpoint");
 	
 	//Tools
 	public static final ItemSword DEEP_CRYSTAL_SWORD = new ToolSword("deep_crystal_sword", MATERIAL_DEEP_CRYSTAL);
