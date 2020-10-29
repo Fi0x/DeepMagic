0e2293a8c15f3cd0527b62f2b1219fe2be059bc0
Fi0x <58072122+Fi0x@users.noreply.github.com>
2020-10-29 17:13:29 +0100

    Feature blocks (#154)
    * Added Insanity Lava
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Update Branch (#153)
    
    * Feature blocks (#147)
    
    * Added charged and breakable spell
    Updated mana tooltips
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Added Mana Altar
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Bug fixes (#150)
    
    * Added insanity blocks to dwarf mining whitelist
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Fixed Demon Summoning Ritual
    Corrected name of demon crystal
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Added explosion spell for mage
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Removed ManaWaster
    Started work on Mage Attack animation
    Updated Todo and polishing lists
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Added mob animation packages for attack and defence animations of mobs
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Improved animation packets for rock troll defence state and synced defence timer
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Fixed Animations for Mage attack and Troll defence
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Bug fixes (#152)
    
    * Added insanity blocks to dwarf mining whitelist
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Fixed Demon Summoning Ritual
    Corrected name of demon crystal
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Added explosion spell for mage
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Removed ManaWaster
    Started work on Mage Attack animation
    Updated Todo and polishing lists
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Added mob animation packages for attack and defence animations of mobs
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Improved animation packets for rock troll defence state and synced defence timer
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Fixed Animations for Mage attack and Troll defence
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Fixed skilltree updating bug
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Corrected Target selection bug with spell and spellstone
    
    Signed-off-by: Fi0x <felpech@aol.com>
    
    * Added weakness and speed effects to insanity water
    Removed Insanity lava due to texture bugs
    
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/blocks/BlockFluid.java
+++ src/main/java/com/fi0x/deepmagic/blocks/BlockFluid.java
@@ -4,8 +4,14 @@ import com.fi0x.deepmagic.init.ModBlocks;
 import com.fi0x.deepmagic.init.ModItems;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 
@@ -29,5 +35,15 @@ public class BlockFluid extends BlockFluidClassic
     public EnumBlockRenderType getRenderType(@Nonnull IBlockState state)
     {
         return EnumBlockRenderType.MODEL;
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
+    {
+        if(entityIn instanceof EntityLivingBase)
+        {
+            ((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 30*20, 0, false, true));
+            ((EntityLivingBase) entityIn).addPotionEffect(new PotionEffect(MobEffects.SPEED, 30*20, 0, false, true));
+        }
     }
 }
