29513afdfbec102169e87bb01f411fdf79ff42f5
Fi0x <felpech@aol.com>
2020-11-04 15:54:51 +0100

    Added TileEntity with inventory for Mana Generator
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/entities/tileentity/TileEntityManaGenerator.java
+++ src/main/java/com/fi0x/deepmagic/entities/tileentity/TileEntityManaGenerator.java
@@ -1,101 +1,224 @@
 package com.fi0x.deepmagic.entities.tileentity;
 
+import com.fi0x.deepmagic.blocks.mana.ManaGenerator;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.event.ForgeEventFactory;
+
+import javax.annotation.Nonnull;
 
 public class TileEntityManaGenerator extends TileEntity implements IInventory, ITickable
 {
+    private NonNullList<ItemStack> inventory = NonNullList.withSize(1, ItemStack.EMPTY);
+    private String customName;
+
+    private int burnTime;
+    private int currentBurnTime;
+
     @Override
     public int getSizeInventory()
     {
-        return 0;
+        return inventory.size();
     }
     @Override
     public boolean isEmpty()
     {
-        return false;
+        for(ItemStack stack : inventory)
+        {
+            if(!stack.isEmpty()) return false;
+        }
+        return true;
     }
+    @Nonnull
     @Override
     public ItemStack getStackInSlot(int index)
     {
-        return null;
+        return inventory.get(index);
     }
+    @Nonnull
     @Override
     public ItemStack decrStackSize(int index, int count)
     {
-        return null;
+        return ItemStackHelper.getAndSplit(inventory, index, count);
     }
+    @Nonnull
     @Override
     public ItemStack removeStackFromSlot(int index)
     {
-        return null;
+        return ItemStackHelper.getAndRemove(inventory, index);
     }
     @Override
     public void setInventorySlotContents(int index, ItemStack stack)
     {
+        ItemStack itemStack = inventory.get(index);
+        boolean flag = !stack.isEmpty() && stack.isItemEqual(itemStack) && ItemStack.areItemStackTagsEqual(stack, itemStack);
+        inventory.set(index, stack);
 
+        if(stack.getCount() > getInventoryStackLimit()) stack.setCount(getInventoryStackLimit());
     }
     @Override
     public int getInventoryStackLimit()
     {
-        return 0;
+        return 64;
     }
     @Override
-    public boolean isUsableByPlayer(EntityPlayer player)
+    public boolean isUsableByPlayer(@Nonnull EntityPlayer player)
     {
-        return false;
+        return world.getTileEntity(pos) == this && player.getDistanceSq((double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D) <= 64;
     }
     @Override
-    public void openInventory(EntityPlayer player)
+    public void openInventory(@Nonnull EntityPlayer player)
     {
-
     }
     @Override
-    public void closeInventory(EntityPlayer player)
+    public void closeInventory(@Nonnull EntityPlayer player)
     {
-
     }
     @Override
-    public boolean isItemValidForSlot(int index, ItemStack stack)
+    public boolean isItemValidForSlot(int index, @Nonnull ItemStack stack)
     {
-        return false;
+        return isItemFuel(stack);
     }
     @Override
     public int getField(int id)
     {
+        switch (id)
+        {
+            case 0: return burnTime;
+            case 1: return currentBurnTime;
+        }
         return 0;
     }
     @Override
     public void setField(int id, int value)
     {
-
+        switch (id)
+        {
+            case 0: burnTime = value;
+            break;
+            case 1: currentBurnTime = value;
+            break;
+        }
     }
     @Override
     public int getFieldCount()
     {
-        return 0;
+        return 2;
     }
     @Override
     public void clear()
     {
-
+        inventory.clear();
     }
     @Override
     public void update()
     {
+        boolean wasRunning = false;
+        boolean dirty = false;
+        if(isRunning())
+        {
+            wasRunning = true;
+            burnTime--;
+            dirty = true;
+        }
+        if(world.isRemote) return;
 
+        ItemStack stack = inventory.get(0);
+
+        if(!isRunning())
+        {
+            if(!stack.isEmpty())
+            {
+                burnTime = getItemBurnTime(stack);
+                currentBurnTime = burnTime;
+                stack.shrink(1);
+                inventory.set(0, stack);
+                dirty = true;
+            }
+        }
+        if(isRunning() != wasRunning) ManaGenerator.setState(isRunning(), world, pos);
+        if(dirty) markDirty();
     }
+    @Nonnull
     @Override
     public String getName()
     {
-        return null;
+        return hasCustomName() ? customName : "container.mana_generator";
     }
     @Override
     public boolean hasCustomName()
     {
-        return false;
+        return customName != null && !customName.isEmpty();
+    }
+    @Nonnull
+    @Override
+    public ITextComponent getDisplayName()
+    {
+        return hasCustomName() ? new TextComponentString(getName()) : new TextComponentTranslation(getName());
+    }
+    @Nonnull
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        compound.setInteger("burnTime", burnTime);
+        ItemStackHelper.saveAllItems(compound, inventory);
+        if(hasCustomName()) compound.setString("customName", customName);
+        return super.writeToNBT(compound);
+    }
+    @Override
+    public void readFromNBT(NBTTagCompound compound)
+    {
+        burnTime = compound.getInteger("burnTime");
+        currentBurnTime = getItemBurnTime(inventory.get(0));
+        inventory = NonNullList.withSize(getSizeInventory(), ItemStack.EMPTY);
+        ItemStackHelper.loadAllItems(compound, inventory);
+        if(compound.hasKey("customName")) setCustomName(compound.getString("customName"));
+        super.readFromNBT(compound);
+    }
+    public void setCustomName(String customName)
+    {
+        this.customName = customName;
+    }
+    public boolean isRunning()
+    {
+        return burnTime > 0;
+    }
+    public static int getItemBurnTime(ItemStack fuel)
+    {
+        if(fuel.isEmpty()) return 0;
+
+        Item item = fuel.getItem();
+        if(item instanceof ItemBlock && Block.getBlockFromItem(item) != Blocks.AIR)
+        {
+            Block block = Block.getBlockFromItem(item);
+            if(block == Blocks.WOODEN_SLAB) return 150;
+            if(block.getDefaultState().getMaterial() == Material.WOOD) return 300;
+            if(block == Blocks.COAL_BLOCK) return 16000;
+        }
+        if(item == Items.STICK) return 100;
+        if(item == Items.COAL) return 1600;
+        if(item == Items.LAVA_BUCKET) return 20000;
+        if(item == Item.getItemFromBlock(Blocks.SAPLING)) return 100;
+        if(item == Items.BLAZE_ROD) return 2400;
+        return ForgeEventFactory.getItemBurnTime(fuel);
+    }
+    public static boolean isItemFuel(ItemStack fuel)
+    {
+        return getItemBurnTime(fuel) > 0;
     }
 }
