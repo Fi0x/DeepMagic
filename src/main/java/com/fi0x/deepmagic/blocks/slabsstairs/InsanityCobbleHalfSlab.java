51107155a2dc3a055a5a8d43920fbed524a3f36d
Fi0x <felpech@aol.com>
2020-11-04 14:06:12 +0100

    Added insanity cobble stairs and slabs
    Signed-off-by: Fi0x <felpech@aol.com>

---
+++ src/main/java/com/fi0x/deepmagic/blocks/slabsstairs/InsanityCobbleHalfSlab.java
@@ -0,0 +1,40 @@
+package com.fi0x.deepmagic.blocks.slabsstairs;
+
+import com.fi0x.deepmagic.Main;
+import com.fi0x.deepmagic.init.DeepMagicTab;
+import com.fi0x.deepmagic.init.ModBlocks;
+import com.fi0x.deepmagic.util.IHasModel;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+public class InsanityCobbleHalfSlab extends SlabBase implements IHasModel
+{
+    public InsanityCobbleHalfSlab(String name, Material materialIn)
+    {
+        super(name, materialIn);
+        setCreativeTab(DeepMagicTab.BLOCKS);
+    }
+
+    @Override
+    public void registerModels()
+    {
+        Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+
+    @Override
+    public boolean isDouble()
+    {
+        return false;
+    }
+
+    @Nonnull
+    @Override
+    public Item getItemDropped(@Nonnull IBlockState state, @Nonnull Random rand, int fortune)
+    {
+        return Item.getItemFromBlock(ModBlocks.INSANITY_COBBLE_SLAB_HALF);
+    }
+}
