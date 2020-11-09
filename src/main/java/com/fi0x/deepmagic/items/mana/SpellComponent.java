f85633152b3a584820808eacc7eab75d129fce6a
Fi0x <felpech@aol.com>
2020-11-09 09:54:23 +0100

    Finished Spell Component Tooltips
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/items/mana/SpellComponent.java
+++ src/main/java/com/fi0x/deepmagic/items/mana/SpellComponent.java
@@ -28,10 +28,73 @@ public class SpellComponent extends ItemBase
         compound = stack.getTagCompound();
         assert compound != null;
 
-        if(compound.hasKey("target"))
+        if(compound.hasKey("effect"))
         {
-            if(compound.getByte("target") == 5) tooltip.add(TextFormatting.GREEN + "Target is 5" );
-            else tooltip.add(TextFormatting.YELLOW + "Target Tag found, but not 5");
-        } else tooltip.add(TextFormatting.RED + "No Target tag found" );
+            tooltip.add(TextFormatting.GREEN + "Effect Component");
+            switch(compound.getByte("effect"))
+            {
+                case 0:
+                    tooltip.add(TextFormatting.GREEN + "Damage");
+                    break;
+                case 1:
+                    tooltip.add(TextFormatting.GREEN + "Environmental Damage");
+                    break;
+                case 2:
+                    tooltip.add(TextFormatting.GREEN + "Explosion");
+                    break;
+                case 3:
+                    tooltip.add(TextFormatting.GREEN + "Heal");
+                    break;
+                case 4:
+                    tooltip.add(TextFormatting.GREEN + "Time");
+                    break;
+                case 5:
+                    tooltip.add(TextFormatting.GREEN + "Weather");
+                    break;
+                default:
+                    tooltip.add(TextFormatting.RED + "Unknown Effect" );
+                    break;
+            }
+        } else if(compound.hasKey("modifier"))
+        {
+            tooltip.add(TextFormatting.WHITE + "Modifier Component");
+            switch(compound.getByte("modifier"))
+            {
+                case 0:
+                    tooltip.add(TextFormatting.WHITE + "Range");
+                    break;
+                case 1:
+                    tooltip.add(TextFormatting.WHITE + "Radius");
+                    break;
+                default:
+                    tooltip.add(TextFormatting.RED + "Unknown Modifier" );
+                    break;
+            }
+
+        } else if(compound.hasKey("target"))
+        {
+            tooltip.add(TextFormatting.YELLOW + "Target Component");
+            switch(compound.getByte("target"))
+            {
+                case 0:
+                    tooltip.add(TextFormatting.YELLOW + "Self" );
+                    break;
+                case 1:
+                    tooltip.add(TextFormatting.YELLOW + "Current Position" );
+                    break;
+                case 2:
+                    tooltip.add(TextFormatting.YELLOW + "Focused Entity" );
+                    break;
+                case 3:
+                    tooltip.add(TextFormatting.YELLOW + "Focused Block" );
+                    break;
+                default:
+                    tooltip.add(TextFormatting.RED + "Unknown Target" );
+                    break;
+            }
+        } else
+        {
+            tooltip.add(TextFormatting.RED + "Unknown Component" );
+        }
     }
 }
