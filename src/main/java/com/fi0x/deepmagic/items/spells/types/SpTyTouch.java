5fa297cdf960f7e7e1d4b58e934065de0cb9f49f
Fi0x <felpech@aol.com>
2021-01-03 18:54:15 +0100

    Implemented "Touch" spell type
    Testing required since client-server sync might not work
    
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/items/spells/types/SpTyTouch.java
+++ src/main/java/com/fi0x/deepmagic/items/spells/types/SpTyTouch.java
@@ -1,8 +1,12 @@
 package com.fi0x.deepmagic.items.spells.types;
 
+import com.fi0x.deepmagic.items.spells.CastHelper;
 import com.fi0x.deepmagic.items.spells.ISpellPart;
+import com.fi0x.deepmagic.items.spells.effects.ISpellEffect;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 
 import java.util.ArrayList;
 
@@ -24,6 +28,40 @@ public class SpTyTouch implements ISpellType
     public void execute(ArrayList<ISpellPart> applicableParts, ArrayList<ArrayList<ISpellPart>> remainingSections, BlockPos castLocation, EntityLivingBase caster)
     {
         applicableParts.remove(0);
-        //TODO: Execute spell
+        boolean executed = false;
+        BlockPos targetPos = getTouchedPos(caster, castLocation);
+
+        while(!applicableParts.isEmpty())
+        {
+            if(applicableParts.get(0) instanceof ISpellEffect)
+            {
+                ((ISpellEffect) applicableParts.get(0)).applyEffect(caster, targetPos, caster.world);
+            } else if(applicableParts.get(0) instanceof ISpellType)
+            {
+                ((ISpellType) applicableParts.get(0)).execute(applicableParts, remainingSections, targetPos, caster);
+                executed = true;
+            }
+            if(executed) break;
+            applicableParts.remove(0);
+        }
+
+        if(!executed)
+        {
+            new CastHelper().findAndCastNextSpellType(remainingSections, targetPos, caster);
+        }
+    }
+
+    private BlockPos getTouchedPos(EntityLivingBase caster, BlockPos castLocation)
+    {
+        BlockPos ret = castLocation;
+
+        RayTraceResult lookingAt = Minecraft.getMinecraft().objectMouseOver;
+        if(lookingAt != null && lookingAt.typeOfHit == RayTraceResult.Type.BLOCK)
+        {
+            ret = lookingAt.getBlockPos();
+            //TODO: Check if server-client sync works
+        }
+
+        return ret;
     }
 }
