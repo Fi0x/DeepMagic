748553c04b5f46a04a3914c348499be32952dbc8
Fi0x <felpech@aol.com>
2021-01-08 11:07:26 +0100

    Added DwarfBaseMarker to identify dwarf home position
    Signed-off-by: Fi0x <felpech@aol.com>

---
+++ src/main/java/com/fi0x/deepmagic/blocks/DwarfBaseMarker.java
@@ -0,0 +1,40 @@
+package com.fi0x.deepmagic.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+
+public class DwarfBaseMarker extends BlockBase
+{
+    public DwarfBaseMarker(String name, Material material)
+    {
+        super(name, material);
+        setSoundType(SoundType.STONE);
+        setHardness(5.0F);
+        setResistance(5.0F);
+        setHarvestLevel("pickaxe", 2);
+        setLightLevel(0.5F);
+        setTickRandomly(true);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(@Nonnull IBlockState stateIn, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull Random rand)
+    {
+        if(worldIn.getBlockState(pos.up()).getCollisionBoundingBox(worldIn, pos.up()) == null)
+        {
+            double d0 = (double) pos.getX() + Math.random();
+            double d1 = (double) pos.getY() + 1;
+            double d2 = (double) pos.getZ() + Math.random();
+
+            worldIn.spawnParticle(EnumParticleTypes.FIREWORKS_SPARK, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+        }
+    }
+}
