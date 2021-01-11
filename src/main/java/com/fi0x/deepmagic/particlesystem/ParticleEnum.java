f021221b8ef21ae324e7a794b4f71497cd10df3f
Fi0x <felpech@aol.com>
2021-01-11 12:42:15 +0100

    Added particle for spell projectile
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/particlesystem/ParticleEnum.java
+++ src/main/java/com/fi0x/deepmagic/particlesystem/ParticleEnum.java
@@ -2,12 +2,14 @@ package com.fi0x.deepmagic.particlesystem;
 
 import com.fi0x.deepmagic.particlesystem.particles.ParticleDwarfSearchMine;
 import com.fi0x.deepmagic.particlesystem.particles.ParticleMagicLight;
+import com.fi0x.deepmagic.particlesystem.particles.ParticleSpellProjectile;
 import com.fi0x.deepmagic.util.handlers.ConfigHandler;
 
 public enum ParticleEnum
 {
     MAGIC_LIGHT("magic_light", ConfigHandler.firstParticleID, true, ParticleMagicLight.class),
-    DWARF_SEARCH_MINE("dwarf_search_mine", ConfigHandler.firstParticleID + 1, false, ParticleDwarfSearchMine.class);
+    DWARF_SEARCH_MINE("dwarf_search_mine", ConfigHandler.firstParticleID + 1, false, ParticleDwarfSearchMine.class),
+    SPELL_PROJECTILE("spell_projectile", ConfigHandler.firstParticleID + 2, false, ParticleSpellProjectile.class);
 
     private final Class particleClass;
     private final String textureName;
