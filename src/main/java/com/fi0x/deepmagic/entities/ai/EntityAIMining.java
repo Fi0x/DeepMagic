d8a240f734560c9bf55c19a9f9fd1754a2c54510
Fi0x <felpech@aol.com>
2021-01-15 14:11:27 +0100

    Added check for depth dimension
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/entities/ai/EntityAIMining.java
+++ src/main/java/com/fi0x/deepmagic/entities/ai/EntityAIMining.java
@@ -66,7 +66,7 @@ public class EntityAIMining extends EntityAIBase
         if(this.entity.getIdleTime() >= 100 || this.entity.getRNG().nextInt(this.executionChance) != 0) return false;
 
         if(!AIHelperMining.hasHomePosition(world, entity)) return false;
-        return entity.posY < ConfigHandler.dwarfMaxMiningHeight;
+        return entity.posY < ConfigHandler.dwarfMaxMiningHeight || entity.dimension == 43;//TODO: Use config-handler value
     }
     @Override
     public void startExecuting()
