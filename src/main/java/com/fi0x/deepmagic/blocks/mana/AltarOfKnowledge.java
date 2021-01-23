7aa0f9fb2ccc9cce28b3e57ab634d8a3a7fa2d08
Fi0x <58072122+Fi0x@users.noreply.github.com>
2021-01-23 16:22:36 +0100

    Particles (#244)
    * Added particles to altar of knowledge, mana altar, mana grinder and mana infuser
    
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/blocks/mana/AltarOfKnowledge.java
+++ src/main/java/com/fi0x/deepmagic/blocks/mana/AltarOfKnowledge.java
@@ -5,6 +5,7 @@ import com.fi0x.deepmagic.blocks.BlockBase;
 import com.fi0x.deepmagic.mana.player.PlayerMana;
 import com.fi0x.deepmagic.mana.player.PlayerProperties;
 import com.fi0x.deepmagic.network.PacketGetSkill;
+import com.fi0x.deepmagic.util.handlers.ConfigHandler;
 import com.fi0x.deepmagic.util.handlers.PacketHandler;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -12,10 +13,14 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
+import java.util.Random;
 
 public class AltarOfKnowledge extends BlockBase
 {
@@ -39,5 +44,18 @@ public class AltarOfKnowledge extends BlockBase
             Main.proxy.openSkilltreeGui(playerIn);
         }
         return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(@Nonnull IBlockState stateIn, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull Random rand)
+    {
+        if(rand.nextInt(100) + 1 > ConfigHandler.altarOfKnowledgeParticles) return;
+
+        double x = pos.getX() + (Math.random());
+        double y = pos.getY() + 1 + (Math.random() * 0.2);
+        double z = pos.getZ() + (Math.random());
+
+        worldIn.spawnParticle(EnumParticleTypes.ENCHANTMENT_TABLE, x, y, z, 0, 1, 0);
+
     }
 }
--- src/main/java/com/fi0x/deepmagic/util/handlers/ConfigHandler.java
+++ src/main/java/com/fi0x/deepmagic/util/handlers/ConfigHandler.java
@@ -129,9 +129,11 @@ public class ConfigHandler
     public static int magicLightParticles;
     public static int deepCrystalOreParticles;
     public static int manaGeneratorParticles;
+    public static int manaConsumerParticles;
     public static int plantParticles;
     public static int demonStoneParticles;
     public static int spellStoneParticles;
+    public static int altarOfKnowledgeParticles;
 
     //Mana Costs
     public static int manaBoosterAmount;
@@ -342,9 +344,11 @@ public class ConfigHandler
         magicLightParticles = blocks.getInt("Magic Light Particles", category, 12, 0, 64, "The amount of Particles for Graphics: 'All Particles' ('Decreased Particles' uses half)");
         deepCrystalOreParticles = blocks.getInt("Deep Crystal Ore Particles", category, 20, 0, 100, "The Chance for Particles to spawn around Deep Crystal Ores");
         manaGeneratorParticles = blocks.getInt("Mana Generator Particles", category, 20, 0, 100, "The Chance for Particles to spawn around Mana Generators");
+        manaConsumerParticles = blocks.getInt("Mana Consumer Particles", category, 10, 0, 100, "The Chance for Particles to spawn around Mana consuming Blocks");
         plantParticles = blocks.getInt("Plant Particles", category, 10, 0, 100, "The chance that Particles will spawn around Plants");
         demonStoneParticles = blocks.getInt("Demon Stone Particles", category, 20, 0, 100, "The Chance that Particles will spawn at a correct Demon Stone Structure");
         spellStoneParticles = blocks.getInt("Spell Stone Particles", category, 50, 0, 100, "The Chance that Particles will spawn at an active Spell Stone");
+        altarOfKnowledgeParticles = blocks.getInt("Altar of Knowledge Particles", category, 50, 0, 100, "The Chance that Particles will spawn at an Altar of Knowledge");
 
         blocks.save();
     }
