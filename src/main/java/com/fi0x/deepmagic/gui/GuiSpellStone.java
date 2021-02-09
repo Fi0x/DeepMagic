ed0f4f0a8a0759899ab76f22950b1256ce134ee9
Fi0x <felpech@aol.com>
2021-02-09 17:17:10 +0100

    Added buttons and labels to spell stone gui
    Signed-off-by: Fi0x <felpech@aol.com>

--- src/main/java/com/fi0x/deepmagic/gui/GuiSpellStone.java
+++ src/main/java/com/fi0x/deepmagic/gui/GuiSpellStone.java
@@ -3,11 +3,15 @@ package com.fi0x.deepmagic.gui;
 import com.fi0x.deepmagic.blocks.containers.ContainerSpellStone;
 import com.fi0x.deepmagic.blocks.mana.tile.TileEntitySpellStone;
 import com.fi0x.deepmagic.util.Reference;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiLabel;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
 
+import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.util.Objects;
 
 public class GuiSpellStone extends GuiContainer
@@ -15,6 +19,17 @@ public class GuiSpellStone extends GuiContainer
     private static final ResourceLocation TEXTURES = new ResourceLocation(Reference.MOD_ID + ":textures/gui/spell_stone.png");
     private final InventoryPlayer player;
     private final TileEntitySpellStone te;
+
+    private int elementID = 1;
+
+    private GuiButton btnStart;
+    private GuiButton btnAddPart;
+    private GuiButton btnClearParts;
+
+    private GuiLabel lblPart1;
+    private GuiLabel lblPart2;
+    private GuiLabel lblPart3;
+    private GuiLabel lblPart4;
 
     public GuiSpellStone(InventoryPlayer player, TileEntitySpellStone tileentity)
     {
@@ -24,6 +39,33 @@ public class GuiSpellStone extends GuiContainer
         te = tileentity;
     }
 
+    @Override
+    public void initGui()
+    {
+        super.initGui();
+        buttonList.clear();
+        labelList.clear();
+
+        btnStart = new GuiButton(nextID(), guiLeft + 147, guiTop + 104, 18, 18, "GO");
+        buttonList.add(btnStart);
+        btnAddPart = new GuiButton(nextID(), guiLeft + 104, guiTop + 19, 27, 18, "Add");
+        buttonList.add(btnAddPart);
+        btnClearParts = new GuiButton(nextID(), guiLeft + 11, guiTop + 104, 45, 18, "Clear");
+        buttonList.add(btnClearParts);
+
+        lblPart1 = new GuiLabel(this.fontRenderer, nextID(), guiLeft + 11, guiTop + 50, 114, 12, 0);
+        lblPart1.addLine("First Part");
+        labelList.add(lblPart1);
+        lblPart2 = new GuiLabel(this.fontRenderer, nextID(), guiLeft + 11, guiTop + 63, 114, 12, 0);
+        lblPart2.addLine("Second Part");
+        labelList.add(lblPart2);
+        lblPart3 = new GuiLabel(this.fontRenderer, nextID(), guiLeft + 11, guiTop + 76, 114, 12, 0);
+        lblPart3.addLine("Third Part");
+        labelList.add(lblPart3);
+        lblPart4 = new GuiLabel(this.fontRenderer, nextID(), guiLeft + 11, guiTop + 89, 114, 12, 0);
+        lblPart4.addLine("Fourth Part");
+        labelList.add(lblPart4);
+    }
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
@@ -44,5 +86,25 @@ public class GuiSpellStone extends GuiContainer
         GlStateManager.color(1, 1, 1, 1);
         mc.getTextureManager().bindTexture(TEXTURES);
         drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+    }
+    @Override
+    protected void actionPerformed(@Nonnull GuiButton button) throws IOException
+    {
+        super.actionPerformed(button);
+
+        if(button == btnStart)
+        {
+            //TODO: Add spell parts to spell
+        } else if(button == btnAddPart)
+        {
+            //TODO: Get part and add to list
+        } else if(button == btnClearParts)
+        {
+            //TODO: Remove all parts and drop items
+        }
+    }
+    private int nextID()
+    {
+        return elementID++;
     }
 }
