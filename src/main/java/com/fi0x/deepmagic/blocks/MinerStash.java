421f0e1f7b4b5f69b7101ed18f10bb3263c030b3
Fi0x <felpech@aol.com>
2021-02-10 12:18:36 +0100

    Added miner stash block and tile entity
    Signed-off-by: Fi0x <felpech@aol.com>

---
+++ src/main/java/com/fi0x/deepmagic/blocks/MinerStash.java
@@ -0,0 +1,62 @@
+package com.fi0x.deepmagic.blocks;
+
+import com.fi0x.deepmagic.Main;
+import com.fi0x.deepmagic.blocks.tileentity.TileEntityMinerStash;
+import com.fi0x.deepmagic.init.ModBlocks;
+import com.fi0x.deepmagic.util.handlers.ConfigHandler;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Random;
+
+public class MinerStash extends BlockBase implements ITileEntityProvider
+{
+    public MinerStash(String name, Material material)
+    {
+        super(name, material);
+        setSoundType(SoundType.WOOD);
+        setHardness(5.0F);
+        setResistance(5.0F);
+        setHarvestLevel("axe", 0);
+    }
+
+    @Override
+    public boolean onBlockActivated(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state, @Nonnull EntityPlayer playerIn, @Nonnull EnumHand hand, @Nonnull EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if(!worldIn.isRemote) playerIn.openGui(Main.instance, ConfigHandler.guiManaInfuserID, worldIn, pos.getX(), pos.getY(), pos.getZ());//TODO: Use correct gui
+        return true;
+    }
+
+    @Nonnull
+    @Override
+    public Item getItemDropped(@Nonnull IBlockState state, @Nonnull Random rand, int fortune)
+    {
+        return Item.getItemFromBlock(ModBlocks.MINER_STASH);
+    }
+    @Override
+    public void breakBlock(World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state)
+    {
+        TileEntityMinerStash te = (TileEntityMinerStash) worldIn.getTileEntity(pos);
+        assert te != null;
+        InventoryHelper.dropInventoryItems(worldIn, pos, te);
+        super.breakBlock(worldIn, pos, state);
+    }
+    @Nullable
+    @Override
+    public TileEntity createNewTileEntity(@Nonnull World worldIn, int meta)
+    {
+        return new TileEntityMinerStash();
+    }
+}
