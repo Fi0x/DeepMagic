05bfca69ee8cbe66f9b27f0e06e0e06892a5be7e
Fi0x <felpech@aol.com>
2021-12-21 21:06:29 +0100

    Fixed cascading world-gen from dwarf base marker

--- src/main/java/com/fi0x/deepmagic/blocks/tileentity/TileEntityDwarfBaseMarker.java
+++ src/main/java/com/fi0x/deepmagic/blocks/tileentity/TileEntityDwarfBaseMarker.java
@@ -14,27 +14,33 @@ import java.util.List;
 public class TileEntityDwarfBaseMarker extends TileEntity implements ITickable
 {
     private int delay = 200;
+    private int playerActivationRange = 48;
 
     @Override
     public void update()
     {
-        if(!world.isRemote)
-        {
-            delay--;
-            if(delay > 0) return;
-            delay = 200;
+        if(!isActive()) return;
+        if(world.isRemote) return;
+        if(!world.isBlockLoaded(pos)) return;
 
-            AxisAlignedBB aabb = new AxisAlignedBB(pos.add(-16, -5, -16), pos.add(16, 5, 16));
-            List<EntityDwarf> existingDwarfs = world.getEntitiesWithinAABB(EntityDwarf.class, aabb);
-            if(existingDwarfs.size() > 10) return;
+        delay--;
+        if(delay > 0) return;
+        delay = 200;
 
-            if(existingDwarfs.size() < 2 || Math.random() * 1000 + 1 < ConfigHandler.dwarfMarkerSpawnChance) spawnDwarf();
-        }
+        AxisAlignedBB aabb = new AxisAlignedBB(pos.add(-16, -5, -16), pos.add(16, 5, 16));
+        List<EntityDwarf> existingDwarfs = world.getEntitiesWithinAABB(EntityDwarf.class, aabb);
+        if(existingDwarfs.size() > 10) return;
+
+        if(existingDwarfs.size() < 2 || Math.random() * 1000 + 1 < ConfigHandler.dwarfMarkerSpawnChance) spawnDwarf();
+    }
+
+    private boolean isActive()
+    {
+        return world.isAnyPlayerWithinRangeAt((double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, playerActivationRange);
     }
 
     private void spawnDwarf()
     {
-        //TODO: Change to avoid cascading worldgen-lag
         int tries = 0;
         int x = pos.getX();
         int y = pos.getY();
