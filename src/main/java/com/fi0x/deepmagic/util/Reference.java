cb47eb504143ff25cf54ba155395b379483a9e16
Fi0x <felpech@aol.com>
2021-12-22 18:17:04 +0100

    Updated version info

--- src/main/java/com/fi0x/deepmagic/util/Reference.java
+++ src/main/java/com/fi0x/deepmagic/util/Reference.java
@@ -3,7 +3,7 @@ package com.fi0x.deepmagic.util;
 public class Reference {
 	public static final String MOD_ID = "deepmagic";
 	public static final String NAME = "Deep Magic";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.fi0x.deepmagic.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.fi0x.deepmagic.proxy.CommonProxy";
