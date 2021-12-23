8b08c02147fd6c0d4f83f90aa5b28de1780ba3f2
Fi0x <felpech@aol.com>
2021-12-23 12:11:55 +0100

    Added randomness to small dungeon structure blocks

--- src/main/java/com/fi0x/deepmagic/world/generators/dungeon/SmallDungeon.java
+++ src/main/java/com/fi0x/deepmagic/world/generators/dungeon/SmallDungeon.java
@@ -1,26 +1,50 @@
 package com.fi0x.deepmagic.world.generators.dungeon;
 
+import com.fi0x.deepmagic.init.ModBlocks;
 import com.fi0x.deepmagic.util.handlers.ConfigHandler;
+import com.fi0x.deepmagic.world.CustomTemplateProcessor;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraft.world.gen.structure.template.Template;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class SmallDungeon extends WorldGenerator implements IWorldGenerator
 {
-    private final DungeonRoom[] rooms = new DungeonRoom[]{
-            new DungeonRoom("small_dungeon_room0", 7),
-            new DungeonRoom("small_dungeon_room1", 6),
-            new DungeonRoom("small_dungeon_room2", 6),
-            new DungeonRoom("small_dungeon_room3", 8),
-            new DungeonRoom("small_dungeon_room4", 7),
-            new DungeonRoom("small_dungeon_room5", 6)};
-    private final DungeonRoom ceiling = new DungeonRoom("small_dungeon_ceiling", 1);
+    private final DungeonRoom[] rooms;
+    private final DungeonRoom ceiling;
+
+    public SmallDungeon()
+    {
+        ArrayList<IBlockState> replaceableBlocks = new ArrayList<>();
+        replaceableBlocks.add(ModBlocks.DUNGEON_BRICK.getDefaultState());
+
+        Map<Float, Template.BlockInfo> replacementBlocks = new HashMap<>();
+        replacementBlocks.put(0.05F, CustomTemplateProcessor.blockToInfo(ModBlocks.DUNGEON_COBBLE_MOSSY.getDefaultState()));
+        replacementBlocks.put(0.1F, CustomTemplateProcessor.blockToInfo(ModBlocks.DUNGEON_COBBLE.getDefaultState()));
+        replacementBlocks.put(0.15F, CustomTemplateProcessor.blockToInfo(ModBlocks.DUNGEON_STONE.getDefaultState()));
+        replacementBlocks.put(0.25F, CustomTemplateProcessor.blockToInfo(ModBlocks.DUNGEON_BRICK_CRACKED.getDefaultState()));
+        replacementBlocks.put(0.3F, CustomTemplateProcessor.blockToInfo(ModBlocks.DUNGEON_BRICK_MOSSY.getDefaultState()));
+
+        rooms = new DungeonRoom[]{
+                new DungeonRoom("small_dungeon_room0", 7, replaceableBlocks, replacementBlocks),
+                new DungeonRoom("small_dungeon_room1", 6, replaceableBlocks, replacementBlocks),
+                new DungeonRoom("small_dungeon_room2", 6, replaceableBlocks, replacementBlocks),
+                new DungeonRoom("small_dungeon_room3", 8, replaceableBlocks, replacementBlocks),
+                new DungeonRoom("small_dungeon_room4", 7, replaceableBlocks, replacementBlocks),
+                new DungeonRoom("small_dungeon_room5", 6, replaceableBlocks, replacementBlocks)};
+        ceiling = new DungeonRoom("small_dungeon_ceiling", 1, replaceableBlocks, replacementBlocks);
+
+    }
 
     @Override
     public boolean generate(@Nonnull World world, @Nonnull Random rand, @Nonnull BlockPos position)
