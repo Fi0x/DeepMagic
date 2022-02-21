debaedb1d4fd7afe20c34a80de6c31093629796c
Fi0x <felpech@aol.com>
2022-02-21 14:42:30 +0100

    Added altar of knowledge opened achievement

--- src/main/java/com/fi0x/deepmagic/advancements/ModTriggers.java
+++ src/main/java/com/fi0x/deepmagic/advancements/ModTriggers.java
@@ -4,9 +4,11 @@ public class ModTriggers
 {
     public static final CustomTrigger RIGHT_CLICK_SPELL = new CustomTrigger("right_click_spell");
     public static final CustomTrigger LINK_MANA_RELAY = new CustomTrigger("link_mana_relay");
+    public static final CustomTrigger KNOWLEDGE_UI_OPENED = new CustomTrigger("altar_of_knowledge_ui_opened");
 
     public static final CustomTrigger[] TRIGGER_ARRAY = new CustomTrigger[]{
             RIGHT_CLICK_SPELL,
-            LINK_MANA_RELAY
+            LINK_MANA_RELAY,
+            KNOWLEDGE_UI_OPENED
     };
 }
