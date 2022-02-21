d2128829de0a8265d74152165b193f413f1de0af
Fi0x <felpech@aol.com>
2022-02-21 14:28:25 +0100

    Added mana relay linked achievement

--- src/main/java/com/fi0x/deepmagic/advancements/ModTriggers.java
+++ src/main/java/com/fi0x/deepmagic/advancements/ModTriggers.java
@@ -3,8 +3,10 @@ package com.fi0x.deepmagic.advancements;
 public class ModTriggers
 {
     public static final CustomTrigger RIGHT_CLICK_SPELL = new CustomTrigger("right_click_spell");
+    public static final CustomTrigger LINK_MANA_RELAY = new CustomTrigger("link_mana_relay");
 
     public static final CustomTrigger[] TRIGGER_ARRAY = new CustomTrigger[]{
-            RIGHT_CLICK_SPELL
+            RIGHT_CLICK_SPELL,
+            LINK_MANA_RELAY
     };
 }
